8fcb3b42308eca754654d32fda817e1472d9ccd4
Kat-ou <catherine.detournay2020@campus-eni.fr>
2021-04-08 17:32:50 +0200

    id 2002: Créer une sortie (mise en forme partie 1)

--- src/Form/EventFormType.php
+++ src/Form/EventFormType.php
@@ -7,6 +7,9 @@ use App\Entity\Sortie;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +18,21 @@ class EventFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('startDate')
-            ->add('deadLine')
+            ->add('name', TextType::class, ['label'=>'Nom de la sortie:  '])
+            ->add('startDate', DateType::class, [
+                'label'=>'Date et heure de la sortie:  ',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ],
+                DateTimeType::class, [
+                    'widget' => 'choice',
+            ])
+            ->add('deadLine', DateType::class, [
+                'label'=>"Date limite d'inscription: ",
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+
+            ])
             ->add('duration')
             ->add('maxRegistrations')
             ->add('description')
@@ -27,25 +42,24 @@ class EventFormType extends AbstractType
             ])
             ->add('street', EntityType::class, [
                 'class' => Lieu::class,
-                'mapped' =>false,
+                'mapped' => false,
                 'choice_label' => 'street'
             ])
             ->add('latitude', EntityType::class, [
                 'class' => Lieu::class,
-                'mapped' =>false,
+                'mapped' => false,
                 'choice_label' => 'latitude'
             ])
             ->add('longitude', EntityType::class, [
                 'class' => Lieu::class,
-                'mapped' =>false,
+                'mapped' => false,
                 'choice_label' => 'longitude'
             ])
             ->add('ville', EntityType::class, [
                 'class' => Ville::class,
-                'mapped' =>false,
+                'mapped' => false,
                 'choice_label' => 'name'
-            ])
-        ;
+            ]);
 
     }
 
