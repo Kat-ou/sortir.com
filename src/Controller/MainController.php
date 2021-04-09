103abc70362d721d33ed7b2444c78b24f6eb7237
Joachim44300 <joachim.desousapeixeira2020@campus-eni.fr>
2021-04-09 15:27:29 +0200

    2002 - Créer une sortie (passage vue campus twig)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -87,6 +87,9 @@ class MainController extends AbstractController
         $currentLatitude = $location->getLatitude();
         $currentLongitude = $location->getLongitude();
         $currentLocationCity = $location->getCity();
+        // On récupère le nom du campus
+        $currentCampus = $this->getUser()->getCampus()->getName();
+
         $eventForm = $this->createForm(EventFormType::class, $event);
         $eventForm->get('name')->setData($currentName);
         $eventForm->get('street')->setData($currentStreet);
@@ -104,7 +107,8 @@ class MainController extends AbstractController
             $entityManager->flush();
         }
         return $this->render('main/create.html.twig', [
-            'eventForm' => $eventForm->createView()
+            'eventForm' => $eventForm->createView(),
+            'nomCampus' =>$currentCampus
         ]);
     }
 
