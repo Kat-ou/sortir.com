50fede8f9d5b09ed1c747a4b50b6f2155ee8d56e
Kat-ou <catherine.detournay2020@campus-eni.fr>
2021-04-09 10:20:13 +0200

    ID 2002:Créer une sortie (css + configuration du formulaire)

--- src/Form/EventFormType.php
+++ src/Form/EventFormType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Entity\Ville;
@@ -18,45 +19,67 @@ class EventFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, ['label'=>'Nom de la sortie:  '])
+            ->add('name', TextType::class, ['label' => 'Nom de la sortie:  '])
             ->add('startDate', DateType::class, [
-                'label'=>'Date et heure de la sortie:  ',
+                'label' => 'Date et heure de la sortie:  ',
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd',
-            ],
-                DateTimeType::class, [
-                    'widget' => 'choice',
-            ])
+                ])
             ->add('deadLine', DateType::class, [
-                'label'=>"Date limite d'inscription: ",
+                'label' => "Date limite d'inscription: ",
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd',
-
             ])
-            ->add('duration')
-            ->add('maxRegistrations')
-            ->add('description')
+            ->add('maxRegistrations', null, [
+                'label' => 'Nombre de places: ',
+            ])
+            ->add('duration', null, [
+                'label' => 'Durée: ',
+                'attr' => array(
+                    'placeholder' => 'minutes'
+                )
+            ])
+            ->add('description', null, [
+                'label' => "Description et infos: ",
+            ])
             ->add('location', EntityType::class, [
+                'label' => "Lieu: ",
                 'class' => Lieu::class,
                 'choice_label' => 'name'
             ])
             ->add('street', EntityType::class, [
+                'label' => "Rue: ",
                 'class' => Lieu::class,
                 'mapped' => false,
                 'choice_label' => 'street'
             ])
             ->add('latitude', EntityType::class, [
+                'label' => "Latitude: ",
                 'class' => Lieu::class,
                 'mapped' => false,
                 'choice_label' => 'latitude'
             ])
             ->add('longitude', EntityType::class, [
+                'label' => "Longitude: ",
                 'class' => Lieu::class,
                 'mapped' => false,
                 'choice_label' => 'longitude'
             ])
             ->add('ville', EntityType::class, [
+                'label' => "Ville: ",
                 'class' => Ville::class,
+                'mapped' => false,
+                'choice_label' => 'name'
+            ])
+            ->add('postcode', EntityType::class, [
+                'label' => "Code Postal: ",
+                'class' => Ville::class,
+                'mapped' => false,
+                'choice_label' => 'postcode'
+            ])
+            ->add('campus', EntityType::class, [
+                'label' => "Campus: ",
+                'class' => Campus::class,
                 'mapped' => false,
                 'choice_label' => 'name'
             ]);
