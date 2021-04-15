61f065b4d9842f1ea3a5d6c07b1cf580b7563608
Kat-ou <catherine.detournay2020@campus-eni.fr>
2021-04-15 19:26:34 +0200

    Id 1009: Supprimer + modifier des utilisateurs (ajout /admin sur les routes)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -25,12 +25,12 @@ use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 //Test php bin/console make:crud
 
 /**
- * @Route("/administration")
+ * @Route("/manage")
  */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/", name="admin_index", methods={"GET"})
+     * @Route("/admin", name="admin_index", methods={"GET"})
      */
     public function index(ParticipantRepository $participantRepository): Response
     {
@@ -42,7 +42,7 @@ class AdminController extends AbstractController
     // non utilisé - voir Registration Controller
 
     /**
-     * @Route("/new", name="admin_new", methods={"GET","POST"})
+     * @Route("/admin/new", name="admin_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
@@ -65,7 +65,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="admin_show", methods={"GET"})
+     * @Route("/admin/{id}", name="admin_show", methods={"GET"})
      */
     public function show(Participant $participant): Response
     {
@@ -75,7 +75,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="admin_edit", methods={"GET","POST"})
+     * @Route("/admin/{id}/edit", name="admin_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
     {
@@ -100,7 +100,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="admin_delete", methods={"POST"})
+     * @Route("/admin/{id}", name="admin_delete", methods={"POST"})
      */
     public function delete(Request $request, Participant $participant): Response
     {
@@ -111,63 +111,5 @@ class AdminController extends AbstractController
         }
 
         return $this->redirectToRoute('admin_index');
-    }
-
-    /**
-     * @Route("/admin/register", name="app_register")
-     */
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, ParticipantRepository $participantRepository,
-                             GuardAuthenticatorHandler $guardHandler, AppAuthenticator $authenticator, CampusRepository $campusRepository, ImportParticipants $importParticipants): Response
-    {
-        // Gestion Formulaire de creation d'un participant :
-        $user = new Participant();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        // Gestion Formulaire d'import de plusieurs participants :
-        $csvForm =  new CsvForm();
-        $csvRegisterForm = $this->createForm(CsvFormType::class, $csvForm);
-        $csvRegisterForm->handleRequest($request);
-
-        // Soumission du formulaire de création d'un seul participant
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword( $passwordEncoder->encodePassword( $user, "password" ) );
-            // Choisir si les comptes sont actifs ou inactifs lors de la création
-            $user->setCreatedDate(new \DateTime());
-            $user->setRoles([$form->get("roles")->getData()]);
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // Message flash d'aide à l'utilisateur
-            $this->addFlash("link", "L'utilisateur a été créée");
-            return $this->redirectToRoute('app_register');
-        }
-
-        // Soumission du formulaire de création d'un seul participant
-        if ( $csvRegisterForm->isSubmitted() && $csvRegisterForm->isValid() ) {
-            // On charge le fichier csv dans notre répertoire
-            $isItUploaded = $importParticipants->uploadCsvFile($csvRegisterForm);
-            // on lit le fichier s'il est uploadé
-            if ($isItUploaded) {
-                // on insert toutes les données en base
-                $data = $importParticipants->insertParticipantsFromCsvFile();
-                // on efface le fichier
-                $importParticipants->deleteCsvFile();
-                // on traite les résultats des insertions
-                if ($data['errorInsert'] === "" && count($data['errorParticipants']) === 0) {
-                    $this->addFlash("info", "Les " . $data['nbInsert'] . " participants ont été importé avec succès");
-                } else {
-                    $nbInsertDone = $data['nbInsert'] - $data['nbErrors'];
-                    $this->addFlash("info", $nbInsertDone . " participant(es) d'inséré(es) - Erreur sur le(s) participant(es) : " . implode(' | ', $data['errorParticipants']));
-                }
-            } else {
-                $this->addFlash("danger", "Le téléchargement du fichier a échoué.");
-            }
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-            'csvForm' => $csvRegisterForm->createView(),
-        ]);
     }
 }
